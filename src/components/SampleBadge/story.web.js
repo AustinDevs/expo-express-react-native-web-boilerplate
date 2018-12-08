d4a9f4b72524c492e6a1b8c87f8e830c1b8511e9
Kevin Colten <kevincolten@gmail.com>
2018-12-08 11:33:12 -0600

    few components

---
+++ src/components/SampleBadge/story.web.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import SampleBadge from '.';
+
+storiesOf('SampleBadge', module)
+  .add('Default', () =>
+    <SampleBadge
+      text='42'
+    />
+  )
+  .add('Blue', () =>
+    <SampleBadge
+      text='B'
+      backgroundColor='blue'
+      textColor='white'
+    />
+  );
---
+++ src/components/SampleButton/story.native.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+import SampleButton from '.';
+
+storiesOf('SampleButton', module)
+  .add('Default', () =>
+    <SampleButton
+      onPress={action('hello')}
+      title='Default'
+    />
+  )
+  .add('Blue', () =>
+    <SampleButton
+      onPress={() => action('hello')}
+      title='Default'
+      color='blue'
+    />
+  );;
